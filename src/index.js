3617d201ee9b32ad9c9f99de9866784f7a2d49ae
Denis Lopatin <stalk.sdv@gmail.com>
2018-05-04 15:53:39 +0300

    eslint setup

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import path from 'path';
 
 const app = express();
 
-app.post('/api/auth', (req, res) =>{
-    res.status(400).json({errors: {global: 'Invalid credentials'}});
-})
+app.post('/api/auth', (req, res) => {
+    res.status(400).json({errors: {global: 'Неправильные данные'}});
+});
 
-app.get('/*', (req,res)=>{
+app.get('/*', (req,res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-})
+});
 
-app.listen(8080, () => console.log('Running on loacalhost:8080'));
+app.listen(8606, () => console.log('Running on loacalhost:8606'));
